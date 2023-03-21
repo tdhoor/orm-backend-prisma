e58e01edf3722a85ca2277bf220dc6055f96798a
Thomas Josef Dorfer <2110738536@stud.fh-kufstein.ac.at>
2023-03-22 00:16:35 +0100

    updated to mysql

---
+++ prisma/migrations/20230321230234_v1/migration.sql
@@ -0,0 +1,83 @@
+-- CreateTable
+CREATE TABLE `Address` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `street` VARCHAR(100) NOT NULL,
+    `city` VARCHAR(100) NOT NULL,
+    `zipCode` VARCHAR(20) NOT NULL,
+    `country` VARCHAR(100) NOT NULL,
+    `customerId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Address_customerId_key`(`customerId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Customer` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(500) NOT NULL,
+    `phone` VARCHAR(50) NOT NULL,
+    `password` VARCHAR(500) NOT NULL,
+    `firstName` VARCHAR(50) NOT NULL,
+    `lastName` VARCHAR(50) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Order` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `totalPrice` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `customerId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `OrderItem` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quantity` INTEGER NOT NULL,
+    `orderId` INTEGER NULL,
+    `productId` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ProductCategory` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Product` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(100) NOT NULL,
+    `price` DOUBLE NOT NULL,
+    `description` TEXT NOT NULL,
+    `productCategoryId` INTEGER NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Address` ADD CONSTRAINT `Address_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Order` ADD CONSTRAINT `Order_customerId_fkey` FOREIGN KEY (`customerId`) REFERENCES `Customer`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `Order`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `OrderItem` ADD CONSTRAINT `OrderItem_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Product` ADD CONSTRAINT `Product_productCategoryId_fkey` FOREIGN KEY (`productCategoryId`) REFERENCES `ProductCategory`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- src/functions/delete-all-entities.function.ts
+++ src/functions/delete-all-entities.function.ts
@@ -2,10 +2,11 @@ import { Prisma } from "@prisma/client";
 import { DB } from "../db";
 
 export async function deleteAllEntities() {
-    const tables = Prisma.ModelName;
-    const tableNames = Object.keys(tables).join(`","`);
-    const query = `TRUNCATE "${tableNames}" RESTART IDENTITY CASCADE;`
-    console.log("delete all " + query);
-    await DB.$executeRawUnsafe(query)
+    const tables = Object.values(Prisma.ModelName);
+    for (const table of tables) {
+        await DB.$executeRawUnsafe('SET FOREIGN_KEY_CHECKS = 0;');
+        await DB.$executeRawUnsafe('TRUNCATE table `' + table + '`;');
+        await DB.$executeRawUnsafe('SET FOREIGN_KEY_CHECKS = 1;');
+    }
     console.log("delete all done");
 }
