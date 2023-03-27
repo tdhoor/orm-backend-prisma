1ab3a29055e0c35099f4c109ea1ac54711064b95
Thomas Josef Dorfer <2110738536@stud.fh-kufstein.ac.at>
2023-03-27 16:23:36 +0200

    removed console.log from delete-all-entities function

--- src/functions/delete-all-entities.function.ts
+++ src/functions/delete-all-entities.function.ts
@@ -4,9 +4,7 @@ import { DB } from "../db";
 export async function deleteAllEntities() {
     const tables = Object.values(Prisma.ModelName);
     for (const tableName of tables) {
-        console.log("delete: " + tableName);
         await DB.$executeRawUnsafe(`DELETE from [${tableName}];`)
         await DB.$executeRawUnsafe(`DBCC CHECKIDENT ([${tableName}], RESEED, 0);`);
     }
-    console.log("delete all done");
 }
