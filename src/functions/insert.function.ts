57dd9b8ce15ad26a312867da3d41e059e743933a
tdhoor <info@thomasdorfer.com>
2023-04-16 17:58:30 +0200

    ended mysql pool after usage

--- src/functions/insert.function.ts
+++ src/functions/insert.function.ts
@@ -1,12 +1,13 @@
 import { bulkInsertMysql } from "@core/functions/bulk-insert-mysql.function";
 import mysql from "mysql2";
 
-export function insert<T>(tableName, data: any[]) {
+export async function insert<T>(tableName, data: any[]) {
     const pool = mysql.createPool({
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
     });
-    return bulkInsertMysql(pool, tableName, data);
+    await bulkInsertMysql(pool, tableName, data);
+    pool.end();
 }
