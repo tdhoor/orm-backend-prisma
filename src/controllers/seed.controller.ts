ad13983c9e54ea3436f50cf8c42fa841576774c6
tdhoor <info@thomasdorfer.com>
2023-04-18 00:37:27 +0200

    added count route

--- src/controllers/seed.controller.ts
+++ src/controllers/seed.controller.ts
@@ -59,7 +59,18 @@ async function resetDb(req, res, next) {
     }
 }
 
+async function countAll(req, res, next) {
+    try {
+        const count = await countEntities();
+        res.status(200).json({ message: "Counted", count });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({ message: "Error counting" });
+    }
+}
+
 export default {
     seedDb,
-    resetDb
+    resetDb,
+    countAll
 }
